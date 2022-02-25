c7ff7d5ab360e4c4c26640c83ff8022fd424d72c
kmatsumoto <255119t@gs.kochi-tech.ac.jp>
2022-02-26 01:25:10 +0900

    Fix AutoPersist

--- src/hotspot/share/gc/auto_persist/autoPersistBarrierSet.hpp
+++ src/hotspot/share/gc/auto_persist/autoPersistBarrierSet.hpp
@@ -26,31 +26,34 @@ class AutoPersistBarrierSet: public CardTableBarrierSet {
     // primitive
     template <typename T>
     static T load_in_heap_at(oop base, ptrdiff_t offset) {
-      base->autopersist_nvm_header();
+      // base->autopersist_nvm_header();
       T result = Parent::template load_in_heap_at<T>(base, offset);
       return result;
     }
 
     template <typename T>
     static void store_in_heap_at(oop base, ptrdiff_t offset, T value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       Parent::store_in_heap_at(base, offset, value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
     }
 
     template <typename T>
     static T atomic_xchg_in_heap_at(oop base, ptrdiff_t offset, T new_value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       T result = Parent::atomic_xchg_in_heap_at(base, offset, new_value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
       return result;
     }
 
     template <typename T>
     static T atomic_cmpxchg_in_heap_at(oop base, ptrdiff_t offset, T compare_value, T new_value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       T result = Parent::atomic_cmpxchg_in_heap_at(base, offset, compare_value, new_value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
       return result;
     }
 
@@ -58,39 +61,43 @@ class AutoPersistBarrierSet: public CardTableBarrierSet {
     static void arraycopy_in_heap(arrayOop src_obj, size_t src_offset_in_bytes, T* src_raw,
                                   arrayOop dst_obj, size_t dst_offset_in_bytes, T* dst_raw,
                                   size_t length) {
-      OrderAccess::fence();
-      if (src_obj != NULL) src_obj->autopersist_nvm_header();
-      if (dst_obj != NULL) dst_obj->autopersist_nvm_header();
+RETRY:
       // Store in DRAM.
       Parent::arraycopy_in_heap(src_obj, src_offset_in_bytes, src_raw,
                                 dst_obj, dst_offset_in_bytes, dst_raw,
                                 length);
+      OrderAccess::fence();
+      if (src_obj != NULL && src_obj->autopersist_nvm_header() != 0) goto RETRY;
+      if (dst_obj != NULL && dst_obj->autopersist_nvm_header() != 0) goto RETRY;
     }
 
     // oop
     static oop oop_load_in_heap_at(oop base, ptrdiff_t offset) {
-      base->autopersist_nvm_header();
+      // base->autopersist_nvm_header();
       oop result = Parent::oop_load_in_heap_at(base, offset);
       return result;
     }
 
     static void oop_store_in_heap_at(oop base, ptrdiff_t offset, oop value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       Parent::oop_store_in_heap_at(base, offset, value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
     }
 
     static oop oop_atomic_xchg_in_heap_at(oop base, ptrdiff_t offset, oop new_value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       oop result = Parent::oop_atomic_xchg_in_heap_at(base, offset, new_value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
       return result;
     }
 
     static oop oop_atomic_cmpxchg_in_heap_at(oop base, ptrdiff_t offset, oop compare_value, oop new_value) {
-      OrderAccess::fence();
-      base->autopersist_nvm_header();
+RETRY:
       oop result = Parent::oop_atomic_cmpxchg_in_heap_at(base, offset, compare_value, new_value);
+      OrderAccess::fence();
+      if (base->autopersist_nvm_header() != 0) goto RETRY;
       return result;
     }
 
@@ -98,21 +105,25 @@ class AutoPersistBarrierSet: public CardTableBarrierSet {
     static bool oop_arraycopy_in_heap(arrayOop src_obj, size_t src_offset_in_bytes, T* src_raw,
                                       arrayOop dst_obj, size_t dst_offset_in_bytes, T* dst_raw,
                                       size_t length) {
-      OrderAccess::fence();
+RETRY:
       if (src_obj != NULL) src_obj->autopersist_nvm_header();
       if (dst_obj != NULL) dst_obj->autopersist_nvm_header();
       bool result = Parent::oop_arraycopy_in_heap(src_obj, src_offset_in_bytes, src_raw,
                                                   dst_obj, dst_offset_in_bytes, dst_raw,
                                                   length);
+      OrderAccess::fence();
+      if (src_obj != NULL && src_obj->autopersist_nvm_header() != 0) goto RETRY;
+      if (dst_obj != NULL && dst_obj->autopersist_nvm_header() != 0) goto RETRY;
       return result;
     }
 
     // clone
     static void clone_in_heap(oop src, oop dst, size_t size) {
-      OrderAccess::fence();
-      if (src != NULL) src->autopersist_nvm_header();
-      if (dst != NULL) dst->autopersist_nvm_header();
+RETRY:
       Parent::clone_in_heap(src, dst, size);
+      OrderAccess::fence();
+      if (src != NULL && src->autopersist_nvm_header() != 0) goto RETRY;
+      if (dst != NULL && dst->autopersist_nvm_header() != 0) goto RETRY;
     }
   };
 };

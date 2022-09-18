5792d42de165b5805a32f91d7880f79888b5838c
kmatsumoto <255119t@gs.kochi-tech.ac.jp>
2022-09-18 12:22:35 +0900

    More stack size

--- src/hotspot/share/nvm/nvmWorkListStack.hpp
+++ src/hotspot/share/nvm/nvmWorkListStack.hpp
@@ -9,7 +9,8 @@
 
 class NVMWorkListStack : public CHeapObj<mtNone> {
  private:
-  static const unsigned long _work_list_stack_size = 1024 * 128; // 1 MB
+  //static const unsigned long _work_list_stack_size = 1024 * 128; // 1 MB
+  static const unsigned long _work_list_stack_size = 1024 * 1024 * 1; // 8 MB
 
   oop _stack[_work_list_stack_size];
   unsigned long _top;

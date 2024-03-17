60a2aeda19639f060fd019fc17924ac88186e556
Elliot <to.elliot.cheung@gmail.com>
2024-03-17 16:15:06 +0900

    Bailout unsafeputobject
    It would write to persistent objects indeed.
    However, we cannot find the corresponding field in replica.
    So we diable it in C1.

--- src/hotspot/share/c1/c1_LIRGenerator.cpp
+++ src/hotspot/share/c1/c1_LIRGenerator.cpp
@@ -2376,6 +2376,7 @@ void LIRGenerator::do_UnsafePutObject(UnsafePutObject* x) {
   }
   #ifdef OUR_PERSIST
   // decorators |= OURPERSIST_NEEDS_WUPD;
+  bailout("not now"); return;
   assert((decorators & OURPERSIST_NEEDS_WUPD) == 0, "don't update NVM for unsafePutObject");
   #endif
   access_store_at(decorators, type, src, off.result(), data.result());
